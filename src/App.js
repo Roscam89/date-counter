ac44a084561fca4e19f160b50736e83d52431fe9
Roscam89 <83566347+Roscam89@users.noreply.github.com>
2023-07-08 19:27:12 -0700

    Changes to be committed: 	modified:   public/index.html 	modified:   src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	modified:   src/index.js 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const tDate = new Date();
+  const [step, SetStep] = useState(0);
+  const [count, setCount] = useState(step);
+
+  let res;
+
+  function addDays(date, days) {
+    let result = new Date(date);
+    result.setDate(result.getDate() + days);
+    res = result.toDateString();
+    return result;
+  }
+
+  addDays(tDate, count);
+
+  function handleMinusStep() {
+    SetStep((s) => s - 1);
+  }
+  function handlePlusStep() {
+    SetStep((s) => s + 1);
+  }
+  function handleMinusCount() {
+    setCount((c) => c - step);
+  }
+  function handlePlusCount() {
+    setCount((c) => c + step);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      <div className="App">
+        <div className="step">
+          Step: {step}
+          <button className="stepBtn-" onClick={handleMinusStep}>
+            -
+          </button>
+          <button onClick={handlePlusStep}>+</button>
+        </div>
+        <div>
+          Count: {count}
+          <button onClick={handleMinusCount}>-</button>
+          <button onClick={handlePlusCount}>+</button>
+        </div>
+        <p className="ui">
+          {count === 0 && ""} {count > 1 && count}
+          {count < -1 && count.toString().slice(1)}
+          {count === -1 && "Yesterday was"}
+          {count === 1 && " Tomorrow is "}
+          {count > 1 && " days from today is"}
+          {count < -1 && " days ago was"}
+          {count === 0 && " Today is"} {res}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </>
   );
 }
 
